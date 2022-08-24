02c2653abafcd917830015914fca4e89de4fa4fb
Mauro Reis <mauroacsreis@gmail.com>
2022-08-23 23:14:25 -0300

    Add files via upload

--- deploy.js
+++ deploy.js
@@ -1,20 +1,22 @@
-// deploy code will go here
-const hdWalletProvider = require('@truffle/hdwallet-provider')
+const HDWalletProvider = require('@truffle/hdwallet-provider')
 const Web3 = require('web3')
-const { interface, bytecode } = require('./compile')
 
-const provider = new hdWalletProvider(
+const { abi, evm } = require('./compile')
+
+provider = new HDWalletProvider(
   'delay kite jump tip pen play sound length level bundle memory surge',
   'https://rinkeby.infura.io/v3/02b3b14cb9f843a18ed1b090d9c606b5'
 )
+
 const web3 = new Web3(provider)
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts()
+
   console.log('Attempting to deploy from account', accounts[0])
 
-  const resuklt = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
     .send({ gas: '1000000', from: accounts[0] })
 
   console.log('Contract deployed to', result.options.address)
